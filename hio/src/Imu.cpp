60234fe41e9c72f53327a272fe4b518f34bd7baa
reynaldi aditia <reynaldiaditia220@gmail.com>
2022-12-23 16:21:45 +0700

    fixing imu.cpp

--- hio/src/Imu.cpp
+++ hio/src/Imu.cpp
@@ -1,7 +1,18 @@
 #include "Imu.h"
 
 LIS3DHTR<TwoWire> lis;
-extern TFT_eSPI tft;
+TFT_eSPI tft;
+
+// Constant won't change
+const int readingNumber = 10; // The size of the readings
+
+float currentPitch[readingNumber];
+float previousPitch;
+float total = 0; // The running total
+
+int readIndex = 0; // The index of the current reading
+const long intervalTime = 50; // Interval of the reading
+
 Imu::Imu(){
 }
 
@@ -13,13 +24,49 @@ void Imu::begin(){
    }
    lis.setOutputDataRate(LIS3DHTR_DATARATE_25HZ); // data output rate
    lis.setFullScaleRange(LIS3DHTR_RANGE_2G); // scale range
+   
    tft.begin();
    tft.setRotation(3);
    tft.fillScreen(TFT_BLACK);
+   // tft.setTextColor(TFT_WHITE,TFT_BLACK);
+
+   // Initialize all the readings to 0
+   for (int i=0; i<readingNumber; i++) {
+      currentPitch[i] = 0;
+   }
 }
 
-float Imu::pitch(){
-   float currentPitch;
-   currentPitch = lis.getAccelerationX();
-   return currentPitch;
+float Imu::pitch() {
+   // static float pitchAverage;
+   static float average;
+   // unsigned long previousTime = 0;
+   // unsigned long currentTime = millis();
+   
+   // Check to see if it's time to read pitch
+   // if (currentTime - previousTime >= intervalTime) {
+   // previousTime = currentTime;
+   // Save the last time you read the pitch value.
+   previousPitch = currentPitch[readIndex]; // The last reading value
+   total = total - previousPitch; // Subtract the last reading
+   currentPitch[readIndex] = lis.getAccelerationX();
+   // return currentPitch[readIndex];
+  
+   // Prevent the display too many values
+   if (previousPitch != currentPitch[readIndex]) {
+      // pitchAverage = average;
+      previousPitch = currentPitch[readIndex];
+   }
+   // return pitchAverage;
+   // else return 0;
+   total += currentPitch[readIndex];
+   readIndex++;
+   // If we're at the end of the array
+   if (readIndex >= readingNumber) {
+      readIndex = 0; // Wrap around to the beginning
+   }
+   // Smoothing the reading
+   average = total/readingNumber;
+   return average;
+   // }
+   // else return 0;
 }
--- hio/src/main.cpp
+++ hio/src/main.cpp
@@ -1,90 +1,40 @@
 #include "Imu.h"
 #include <Ultrasonic.h>
 
-TFT_eSPI tft;
+extern TFT_eSPI tft;
 // LIS3DHTR<TwoWire> lis;
 Imu imu;
 Ultrasonic ultrasonic(0); // pin D0
 
-// Constant won't change.
+// Constant won't change
 const int pitchReadingNumber = 10; // the size of the readings  
 
-float pitch[pitchReadingNumber]; // analog input
-int pitchIndex = 0; // the index of the current reading 
-float totalPitch = 0; // the running total
-float averagePitch = 0; // the average 
-float previousPitch; // The last reading of the pitch
+const long pitchIntervalTime = 50; // Interval of the reading
+unsigned long previousPitchTime = 0;
 
-const long pitchInterval = 50; // Interval of the reading
-unsigned long previousPitchMillis = 0;
+// The value will quickly become too large.
+unsigned long previousRange1Time = 0;
+const long range1IntervalTime = 250;
 
-/*
-Generally should use "unsigned long" for hadling time.
-The value will quickly become too large.
-*/
-unsigned long previousRange1Millis = 0;
-const long range1Interval = 250;
 long rangeInCentimeters;
 long previousRange1;  // The last reading of the distance
-
-unsigned long previousRollMillis = 0;
-const long rollInterval = 50;
 
 void setup() {
   Serial.begin(115200);
   imu.begin();
-  /*
-  lis.begin(Wire1);
-  if (!lis){
-    Serial.println("ERROR");
-    while(1);
-  }
-  lis.setOutputDataRate(LIS3DHTR_DATARATE_25HZ); // data output rate
-  lis.setFullScaleRange(LIS3DHTR_RANGE_2G); // scale range
-  */
-  tft.begin();
-  tft.setRotation(3);
-  tft.fillScreen(TFT_BLACK); // flicker prevention
-  // initialize all the readings to 0
-  for (int i=0; i<pitchReadingNumber; i++){
-    pitch[i] = 0;
-  }
 }
 
 void loop() {
 
-  unsigned long currentMillis = millis();
-  /*
-  *Read the pitch value
-  Check to see if it's time to read the pitch value.
-  */
-  if (currentMillis - previousPitchMillis >= pitchInterval){
-    // Save the last time you read the pitch value. 
-    previousPitchMillis = currentMillis;
-    previousPitch = pitch[pitchIndex]; // the last reading value 
-    totalPitch = totalPitch - previousPitch; // subtract the last reading
-    pitch[pitchIndex] = imu.pitch();
-    // pitch[pitchIndex] = lis.getAccelerationX(); // Get the pitch values
-    
-    // Prevent the display of too many values
-    if (previousPitch != pitch[pitchIndex]) { // smoothing the readings
-      tft.setTextColor(TFT_WHITE, TFT_BLACK);
-      tft.drawString("x:",60,80,1);
-      tft.drawFloat(averagePitch,1,73,80,1); // Print the pitch value
-      previousPitch = pitch[pitchIndex]; 
-    }
-    totalPitch += pitch[pitchIndex]; // add readings to the total
-    pitchIndex++;
-    // if we're at the end of the array
-    if (pitchIndex >= pitchReadingNumber) {
-      pitchIndex = 0; //wrap around to the beginning
-    }
-    // Calculate the averange
-    averagePitch = totalPitch / pitchReadingNumber;
+  unsigned long currentTime = millis();
+  if (currentTime - previousPitchTime >= pitchIntervalTime){
+    previousPitchTime = currentTime;
+    Serial.println(imu.pitch(),2);
   }
-  // *Read the distance value
-  if (currentMillis - previousRange1Millis >= range1Interval){
-    previousRange1Millis = currentMillis;
+  
+  // Read the distance value
+  if (currentTime - previousRange1Time >= range1IntervalTime){
+    previousRange1Time = currentTime;
     previousRange1 = rangeInCentimeters;
     rangeInCentimeters = ultrasonic.MeasureInCentimeters();
     
